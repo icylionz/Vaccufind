4a01539877a9e6ddb3842e3a04d791742341e585
Icylionz <kinglioniod@gmail.com>
2021-03-29 14:09:57 -0400

    added php files for database and table creations

---
+++ createPatientTable.php
@@ -0,0 +1,41 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// sql to create table
+$sql = "CREATE TABLE patient (
+patientID INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(50) NOT NULL,
+lastname VARCHAR(50) NOT NULL,
+dob DATETIME2,
+streetAddress VARCHAR(100),
+phoneNumber VARCHAR(10),
+email VARCHAR(50),
+tag VARCHAR(50),
+country VARCHAR(25),
+vaccineTypeGiven VARCHAR(35),
+noOfDosesRemaining INT(3),
+appointmentDate DATETIME2,
+medicalConditions VARCHAR(300),
+nid VARCHAR(11),
+passportNumber VARCHAR(30),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Table MyGuests created successfully";
+} else {
+  echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?> 
