3ca0632e45a4bd36b64c90d41cae2eedf37bb51e
Icylionz <kinglioniod@gmail.com>
2021-03-28 21:42:34 -0400

    Added website html elements and css (gui)

---
+++ admin/assets/form/admin_forms.js
@@ -0,0 +1,163 @@
+function forms() {
+    document.getElementById('client').style.display = 'none';
+    document.getElementById('wait').style.display = 'none';
+    document.getElementById('notification').style.display = 'none';
+    document.getElementById('essential').style.display = 'none';
+    document.getElementById('medical').style.display = 'none';
+    document.getElementById('vac').style.display = 'none';
+    document.getElementById('super').style.display = 'none';
+}
+
+function clientList() {
+    if (document.getElementById('client').style.display == 'none') {
+        document.getElementById('default').style.display = 'none';
+        document.getElementById('client').style.display = 'block';
+        document.getElementById('wait').style.display = 'none';
+        document.getElementById('notification').style.display = 'none';
+        document.getElementById('essential').style.display = 'none';
+        document.getElementById('medical').style.display = 'none';
+        document.getElementById('vac').style.display = 'none';
+        document.getElementById('super').style.display = 'none';
+    } else if (document.getElementById('client').style.display == 'block') {
+        document.getElementById('default').style.display = 'block';
+        document.getElementById('client').style.display = 'none';
+        document.getElementById('wait').style.display = 'none';
+        document.getElementById('notification').style.display = 'none';
+        document.getElementById('essential').style.display = 'none';
+        document.getElementById('medical').style.display = 'none';
+        document.getElementById('vac').style.display = 'none';
+        document.getElementById('super').style.display = 'none';
+    }
+}
+
+function waitList() {
+    if (document.getElementById('wait').style.display == 'none') {
+        document.getElementById('default').style.display = 'none';
+        document.getElementById('client').style.display = 'none';
+        document.getElementById('wait').style.display = 'block';
+        document.getElementById('notification').style.display = 'none';
+        document.getElementById('essential').style.display = 'none';
+        document.getElementById('medical').style.display = 'none';
+        document.getElementById('vac').style.display = 'none';
+        document.getElementById('super').style.display = 'none';
+    } else if (document.getElementById('wait').style.display == 'block') {
+        document.getElementById('default').style.display = 'block';
+        document.getElementById('client').style.display = 'none';
+        document.getElementById('wait').style.display = 'none';
+        document.getElementById('notification').style.display = 'none';
+        document.getElementById('essential').style.display = 'none';
+        document.getElementById('medical').style.display = 'none';
+        document.getElementById('vac').style.display = 'none';
+        document.getElementById('super').style.display = 'none';
+    }
+}
+
+function notificationTable() {
+    if (document.getElementById('notification').style.display == 'none') {
+        document.getElementById('default').style.display = 'none';
+        document.getElementById('client').style.display = 'none';
+        document.getElementById('wait').style.display = 'none';
+        document.getElementById('notification').style.display = 'block';
+        document.getElementById('essential').style.display = 'none';
+        document.getElementById('medical').style.display = 'none';
+        document.getElementById('vac').style.display = 'none';
+        document.getElementById('super').style.display = 'none';
+    } else if (document.getElementById('notification').style.display == 'block') {
+        document.getElementById('default').style.display = 'block';
+        document.getElementById('client').style.display = 'none';
+        document.getElementById('wait').style.display = 'none';
+        document.getElementById('notification').style.display = 'none';
+        document.getElementById('essential').style.display = 'none';
+        document.getElementById('medical').style.display = 'none';
+        document.getElementById('vac').style.display = 'none';
+        document.getElementById('super').style.display = 'none';
+    }
+}
+
+function essentialForm() {
+    if (document.getElementById('essential').style.display == 'none') {
+        document.getElementById('default').style.display = 'none';
+        document.getElementById('client').style.display = 'none';
+        document.getElementById('wait').style.display = 'none';
+        document.getElementById('notification').style.display = 'none';
+        document.getElementById('essential').style.display = 'block';
+        document.getElementById('medical').style.display = 'none';
+        document.getElementById('vac').style.display = 'none';
+        document.getElementById('super').style.display = 'none';
+    } else if (document.getElementById('essential').style.display == 'block') {
+        document.getElementById('default').style.display = 'block';
+        document.getElementById('client').style.display = 'none';
+        document.getElementById('wait').style.display = 'none';
+        document.getElementById('notification').style.display = 'none';
+        document.getElementById('essential').style.display = 'none';
+        document.getElementById('medical').style.display = 'none';
+        document.getElementById('vac').style.display = 'none';
+        document.getElementById('super').style.display = 'none';
+    }
+}
+
+function medicalForm() {
+    if (document.getElementById('medical').style.display == 'none') {
+        document.getElementById('default').style.display = 'none';
+        document.getElementById('client').style.display = 'none';
+        document.getElementById('wait').style.display = 'none';
+        document.getElementById('notification').style.display = 'none';
+        document.getElementById('essential').style.display = 'none';
+        document.getElementById('medical').style.display = 'block';
+        document.getElementById('vac').style.display = 'none';
+        document.getElementById('super').style.display = 'none';
+    } else if (document.getElementById('medical').style.display == 'block') {
+        document.getElementById('default').style.display = 'block';
+        document.getElementById('client').style.display = 'none';
+        document.getElementById('wait').style.display = 'none';
+        document.getElementById('notification').style.display = 'none';
+        document.getElementById('essential').style.display = 'none';
+        document.getElementById('medical').style.display = 'none';
+        document.getElementById('vac').style.display = 'none';
+        document.getElementById('super').style.display = 'none';
+    }
+}
+
+function vacForm() {
+    if (document.getElementById('vac').style.display == 'none') {
+        document.getElementById('default').style.display = 'none';
+        document.getElementById('client').style.display = 'none';
+        document.getElementById('wait').style.display = 'none';
+        document.getElementById('notification').style.display = 'none';
+        document.getElementById('essential').style.display = 'none';
+        document.getElementById('medical').style.display = 'none';
+        document.getElementById('vac').style.display = 'block';
+        document.getElementById('super').style.display = 'none';
+    } else if (document.getElementById('vac').style.display == 'block') {
+        document.getElementById('default').style.display = 'block';
+        document.getElementById('client').style.display = 'none';
+        document.getElementById('wait').style.display = 'none';
+        document.getElementById('notification').style.display = 'none';
+        document.getElementById('essential').style.display = 'none';
+        document.getElementById('medical').style.display = 'none';
+        document.getElementById('vac').style.display = 'none';
+        document.getElementById('super').style.display = 'none';
+    }
+}
+
+function superForm() {
+    if (document.getElementById('super').style.display == 'none') {
+        document.getElementById('default').style.display = 'none';
+        document.getElementById('client').style.display = 'none';
+        document.getElementById('wait').style.display = 'none';
+        document.getElementById('notification').style.display = 'none';
+        document.getElementById('essential').style.display = 'none';
+        document.getElementById('medical').style.display = 'none';
+        document.getElementById('vac').style.display = 'none';
+        document.getElementById('super').style.display = 'block';
+    } else if (document.getElementById('super').style.display == 'block') {
+        document.getElementById('default').style.display = 'block';
+        document.getElementById('client').style.display = 'none';
+        document.getElementById('wait').style.display = 'none';
+        document.getElementById('notification').style.display = 'none';
+        document.getElementById('essential').style.display = 'none';
+        document.getElementById('medical').style.display = 'none';
+        document.getElementById('vac').style.display = 'none';
+        document.getElementById('super').style.display = 'none';
+    }
+}
---
+++ admin/assets/form/overlay.js
@@ -0,0 +1,23 @@
+function on1() {
+    document.getElementById("overlay1").style.display = "block";
+}
+
+function off1() {
+    document.getElementById("overlay1").style.display = "none";
+}
+
+function on2() {
+    document.getElementById("overlay2").style.display = "block";
+}
+
+function off2() {
+    document.getElementById("overlay2").style.display = "none";
+}
+
+function on3() {
+    document.getElementById("overlay3").style.display = "block";
+}
+
+function off3() {
+    document.getElementById("overlay3").style.display = "none";
+}
