46b7e5ccb8a5fa10c5ebe2a22ccfa038f4e56a31
Kugaluga04 <kugaluga14@gmail.com>
2021-04-07 16:33:03 -0400

    overlay

--- admin/php/super_admin_logout.php
+++ admin/php/super_admin_logout.php
@@ -1,5 +1,6 @@
 <?php
     unset($_SESSION['username']);
+    unset($_SESSION['passwrd']);
     unset($_SESSION['superAdminFirstName']);
     unset($_SESSION['superAdminLastName']);
     include "admin_logout.php";
